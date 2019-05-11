339da5306460b9956423c950673fa2df4add5dce
Isaac Quintanilla <isaac@iqbiostats.com>
2019-05-11 01:04:09 -0700

    Have a working GIS file for students

--- GIS_prep.R
+++ GIS_prep.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(readxl)
 
 #Loading Data
-vaccine<-read_csv("~/Downloads/kdata14-15.csv") %>% 
+vaccine<-read_csv("~/Other/kdata14-15.csv") %>% 
   select(-c("X12","X13")) %>%
   filter(Reported=="Y") %>% 
   filter(Category %in% c("Conditional","HC Practitioner Counseled PBE","PBE","PME","Pre-Jan PBE","Religious PBE"))
 
-address<-read_excel("~/Downloads/pubschls.xlsx") %>% 
+address<-read_excel("~/Other/pubschls.xlsx") %>% 
   filter(EILCode=="ELEM") %>% 
   select(CDSCode,Latitude,Longitude)
 
@@ -16,13 +16,17 @@ vaccine_wd<-vaccine %>%
   group_by(`School Year`,`School Code`,County,`School Type`,`School Name`) %>%
   summarise(exemption=sum(as.numeric(Count)),enrollment=max(Enrollment)) %>% mutate(School_Code=as.character(`School Code`)) 
 
-address_wd<-address %>% mutate(School_Code=str_sub(CDSCode,9,14))
+address_wd<-address %>% mutate(School_Code=str_sub(CDSCode,9,14)) %>% filter(Latitude != "No Data")
 
 
 #Merging Data
 merged<-left_join(vaccine_wd,address_wd,by="School_Code") %>% drop_na() %>% ungroup() %>%
   select(-c("School Code","CDSCode")) %>%
-  rename(School_Year=`School Year`,School_Type=`School Type`,School_Name=`School Name`)
+  rename(School_Year=`School Year`,School_Type=`School Type`,School_Name=`School Name`) %>%
+  mutate(Latitude=as.numeric(Latitude)) %>% mutate(Longitude=as.numeric(Longitude))
+  
 
 #Exporting 
 write_csv(merged,path = "vaccine_rates")
+
+
